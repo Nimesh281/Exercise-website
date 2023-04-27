191ef7e596896ff46ea5bc535f09e45654823d32
Nimesh <nimesh.sakhiya2003@gmail.com>
2023-04-27 10:15:17 +0530

    landing page half features

---
+++ src/utils/fetchData.js
@@ -0,0 +1,14 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': `c40d9e3223mshc92b2153c4536f5p1a6e50jsndbf17f535a53`
+      // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+    },
+  };
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
